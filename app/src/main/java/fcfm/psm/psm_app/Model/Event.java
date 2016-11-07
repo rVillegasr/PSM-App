733a8aa02d6f55cce611186bf111c8757b359860
Everardo Hernandez D <everardo.hernandezdz@gmail.com>
2016-11-06 21:25:50 -0600

    Integración con el Web Service de los eventos

--- app/src/main/java/fcfm/psm/psm_app/Model/Event.java
+++ app/src/main/java/fcfm/psm/psm_app/Model/Event.java
@@ -1,42 +1,43 @@
 package fcfm.psm.psm_app.Model;
 
-import android.graphics.Bitmap;
-
 import java.sql.Date;
 
 /**
- * Created by RVR_ on 25/09/2016.
+ * Modified by Everardo Hernandez on 25/09/2016.
  */
+
 public class Event {
+    private int id;
     private String name;
     private String description;
     private Date date;
     private String address;
     private String price;
-    private Bitmap img;
-    private Bitmap cover;
+    private String imgPath;
+    private String coverPath;
 
-    public Event(String name, Bitmap img, Bitmap cover) {
-        this.name = name;
-        this.img = img;
-        this.cover = cover;
+    public Event() {
+
     }
 
-    public Event(String name, String description, Bitmap img, Bitmap cover) {
-        this.name = name;
-        this.description = description;
-        this.img = img;
-        this.cover = cover;
-    }
+    public Event(int id, String name, String description, Date date, String address, String price, String imgPath, String coverPath) {
 
-    public Event(String name, String description, Date date, String address, String price, Bitmap img, Bitmap cover) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.date = date;
         this.address = address;
         this.price = price;
-        this.img = img;
-        this.cover = cover;
+        this.imgPath = imgPath;
+        this.coverPath = coverPath;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -53,22 +54,6 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
-    }
-
-    public Bitmap getImg() {
-        return img;
-    }
-
-    public void setImg(Bitmap img) {
-        this.img = img;
-    }
-
-    public Bitmap getCover() {
-        return cover;
-    }
-
-    public void setCover(Bitmap cover) {
-        this.cover = cover;
     }
 
     public Date getDate() {
@@ -93,5 +78,21 @@ public class Event {
 
     public void setPrice(String price) {
         this.price = price;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    public String getCoverPath() {
+        return coverPath;
+    }
+
+    public void setCoverPath(String coverPath) {
+        this.coverPath = coverPath;
     }
 }
---
+++ app/src/main/java/fcfm/psm/psm_app/Model/NetCallback.java
@@ -0,0 +1,9 @@
+package fcfm.psm.psm_app.Model;
+
+/**
+ * Created by everardo on 11/6/2016.
+ */
+
+public interface NetCallback {
+    void onWorkFinish(Object data);
+}
